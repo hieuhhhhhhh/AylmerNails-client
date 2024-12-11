95abd757d8189f583739c9b7d36f95dc2a532e86
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-11 16:28:37 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to the About page</h1>
+  </div>
+  <hello-server />
+</template>
+
+<script>
+import helloServer from "./templates/helloServer.vue";
+export default {
+  components: { helloServer },
+  created() {
+    document.title = "About Us - My Vue App";
+  },
+};
+</script>
---
+++ src/templates/helloServer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  mounted() {
+    fetch("http://127.0.0.1:5000/")
+      .then((response) => response.json())
+      .then((data) => {
+        this.message = data.message;
+      });
+  },
+};
+</script>
