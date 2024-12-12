c8d9eebeccfccb242e94bd5def6b12905b013d6e
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-11 22:52:53 -0500

    started: header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <div><router-link to="/">Home</router-link></div>
     <div><router-link to="/about">About</router-link></div>
     <div><router-link to="/contact">Contact</router-link></div>
@@ -9,11 +10,14 @@
 </template>
 
 <script>
+import Header from "./components/view-shell/Header.vue";
 export default {
+  components: { Header },
   name: "App",
   created() {
     document.title = "Aylmer Nails & Spa";
   },
 };
 </script>
+
 <style src="./global.css"></style>
--- src/components/view-shell/Header.vue
+++ src/components/view-shell/Header.vue
@@ -1,5 +1,20 @@
+<template>
+  <div class="title">Aylmer Nails & Spa</div>
+</template>
 <script>
 export default {
   name: "Header-",
 };
 </script>
+
+<style scoped>
+.title {
+  text-align: center;
+  align-content: center;
+  border-bottom: 1px solid var(--xtrans-gray);
+  box-shadow: 0 0 6px var(--shadow-color);
+  font-size: 30px;
+  height: 50px;
+  background: var(--middleground);
+}
+</style>
