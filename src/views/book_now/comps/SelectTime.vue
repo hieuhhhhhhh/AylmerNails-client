749efd3279c89abb8f4d2e299c541d9f5e039203
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-02-11 00:16:51 -0500

    c

--- src/views/book_now/comps/SelectTime.vue
+++ src/views/book_now/comps/SelectTime.vue
@@ -2,15 +2,15 @@
   <div>
     <input id="date" type="date" :value="date" @change="onInputDate" />
   </div>
-
+  <div v-if="msg">{{ msg }}</div>
   <div v-for="(opening, index) in sortedOpenings" :key="index">
     <button @click="onChooseOpening(opening)">select opening</button>
     <div>{{ formatTime(opening.start) }}</div>
   </div>
 </template>
 <script>
-import getTodayUnixTime from "@/lib/getTodayUnixTime";
 // lib
+import getTodayUnixTime from "@/lib/getTodayUnixTime";
 import fetchAvailability from "../apis/fetchAvailability";
 import secondsToClock from "../apis/secondsToClock";
 import parseDate from "@/lib/parseDate";
@@ -23,12 +23,13 @@ export default {
   data() {
     return {
       // resources
+      msg: "",
       chains: [],
       openings: {},
       sortedOpenings: [],
-      // outcome
       date: null,
-      unixDate: getTodayUnixTime(),
+      // outcome
+      unixDate: null,
     };
   },
   methods: {
@@ -41,34 +42,65 @@ export default {
       const randomChain = opening.chains[random];
       this.onSelectChain(randomChain, this.unixDate);
     },
-    onInputDate(value) {
+    isInThePast(value) {
+      const today = getTodayUnixTime();
+      const validated = parseDate(value);
+      const res = validated <= today - 24 * 60 * 60;
+      return res;
+    },
+    resetData() {
+      this.msg = "";
+      this.chains = [];
+      this.openings = {};
+      this.sortedOpenings = [];
+      this.date = null;
+      this.unixDate = null;
+    },
+    async onInputDate(event) {
+      // reset data
+      this.resetData();
+      // read from event
+      const value = event.target.value;
+      // if user select day in the past do nothing
+      if (this.isInThePast(value)) {
+        this.msg = "Please select a valid date.";
+        return;
+      }
+
       this.date = value;
       this.unixDate = parseDate(value);
+      console.log("unix date: ", this.unixDate);
+      await this.fetchData();
     },
-  },
-  async created() {
-    this.chains = await fetchAvailability(this.date, this.services);
-    console.log(this.chains);
-    // append object 'openings'
-    for (let chain of this.chains) {
-      const start = chain.chainStart;
-      const end = chain.chainEnd;
-      const minEnd = chain.minEnd;
+    async fetchData() {
+      const res = await fetchAvailability(this.unixDate, this.services);
+      if (res) this.chains = res;
+      // append object 'openings'
+      for (let chain of this.chains) {
+        const start = chain.chainStart;
+        const end = chain.chainEnd;
+        const minEnd = chain.minEnd;
 
-      // group chains to groups called 'openings'
-      if (!this.openings[start] || this.openings[start].end > end) {
-        const opening = { start: start, end: end, minEnd: minEnd, chains: [] };
-        this.openings[start] = opening;
+        // group chains to groups called 'openings'
+        if (!this.openings[start] || this.openings[start].end > end) {
+          const opening = {
+            start: start,
+            end: end,
+            minEnd: minEnd,
+            chains: [],
+          };
+          this.openings[start] = opening;
+        }
+
+        if (this.openings[start].end === end) {
+          this.openings[start].chains.push(chain);
+        }
       }
 
-      if (this.openings[start].end === end) {
-        this.openings[start].chains.push(chain);
-      }
-    }
-
-    // convert to array
-    const newOpenings = Object.values(this.openings);
-    this.sortedOpenings = newOpenings.sort((a, b) => a.start - b.start);
+      // convert to array
+      const newOpenings = Object.values(this.openings);
+      this.sortedOpenings = newOpenings.sort((a, b) => a.start - b.start);
+    },
   },
 };
 </script>
