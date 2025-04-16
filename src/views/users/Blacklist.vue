769a08b3eeb08f25b824fcb65a6825213bd2c203
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-04-15 22:18:24 -0400

    w

--- src/views/users/Blacklist.vue
+++ src/views/users/Blacklist.vue
@@ -1,12 +1,143 @@
 <template>
-  <div>blacklist</div>
+  <input
+    id="search"
+    type="text"
+    placeholder="Search..."
+    v-model="query"
+    @input="onSearch"
+  />
+
+  <table>
+    <tbody>
+      <tr>
+        <th class="newCol"></th>
+        <th>Phone Number</th>
+        <th>Banned On</th>
+        <th>Client Name</th>
+        <th>Account Name</th>
+      </tr>
+      <tr class="row" v-for="(row, index) in rows" :key="index">
+        <td class="newCol">
+          <div class="flexBox">
+            <div class="newCell" v-if="row.bannedOn > lastTracked">NEW</div>
+          </div>
+        </td>
+        <td>
+          {{ formatPhone(row.phoneNum) }}
+        </td>
+        <td>
+          {{ unixToReadable(row.bannedOn) }}
+          <div>{{ unixToHours(row.bannedOn) }}</div>
+        </td>
+        <td>
+          {{ row.contactName }}
+        </td>
+        <td>{{ row.firstName }} {{ row.lastName }}</td>
+        <td class="btn">
+          <button class="blueBtn" @click="onUnban(row.phoneNum)">
+            Unblacklist
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
-
-<script setup>
-</script>
-
 <script>
+// lib
+import { onMounted, ref } from "vue";
+import formatPhone from "@/lib/formatPhone";
+import unixToReadable from "@/lib/unixToReadable";
+import unixToHours from "@/lib/unixToHours";
+// apis
+import searchBlacklist from "./apis/searchBlacklist";
+import fetchBlacklistLastTracked from "./apis/fetchBlacklistLastTracked";
+import banUnbanPhoneNum from "./apis/banUnbanPhoneNum";
+import { useRouter } from "vue-router";
+
 export default {
-  name: "Blacklist-",
+  name: "CanceledAppos",
+  setup() {
+    // resoures
+    const query = ref("");
+    const rows = ref([]);
+    const lastTracked = ref(null);
+    const limit = ref(50);
+    // lib
+    const router = useRouter();
+
+    // INPUT
+    const onSearch = async () => {
+      rows.value = await searchBlacklist(query.value, limit.value);
+    };
+
+    const onUnban = async (phoneNum) => {
+      const res = await banUnbanPhoneNum(phoneNum, false);
+      if (res) {
+        router.push("/refresh");
+      }
+    };
+
+    // LIFECYCLE
+    onMounted(async () => {
+      lastTracked.value = await fetchBlacklistLastTracked();
+      await onSearch();
+    });
+
+    return {
+      query,
+      rows,
+      lastTracked,
+      formatPhone,
+      onSearch,
+      unixToReadable,
+      unixToHours,
+      onUnban,
+    };
+  },
 };
 </script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  font-size: 14px;
+}
+th,
+td {
+  padding-top: 10px;
+  padding-bottom: 10px;
+
+  text-align: center;
+}
+tr {
+  border: 1px solid;
+}
+.newCol {
+  padding: 0px;
+  font-size: 8px;
+}
+.flexBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.newCell {
+  padding: 2px;
+  background: var(--trans-red);
+  color: white;
+  border-radius: 2px;
+}
+#search {
+  margin-bottom: 10px;
+}
+.btn {
+  margin: 0;
+  width: 20px;
+}
+button {
+  border-radius: 40px;
+  font-size: 13px;
+}
+</style>
+
