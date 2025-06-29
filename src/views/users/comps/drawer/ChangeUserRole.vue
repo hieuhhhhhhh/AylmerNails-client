7fb968f6ca81a26bdf1343ae3692769d807cf06c
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-06-28 20:43:54 -0400

    n

---
+++ src/views/users/comps/drawer/ChangeUserRole.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="parent">
+    <div id="background" @click="onCancel" />
+    <div id="window">
+      <div id="content">
+        <form @submit.prevent="onSubmit">
+          <div>this is changeee</div>
+          <button>Confirm</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+// apis
+import updateUserRole from "../../apis/updateUserRole";
+
+async function onSubmit() {
+  await updateUserRole();
+}
+</script>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#parent {
+  z-index: 15;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#background {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+#window {
+  position: relative;
+  z-index: 20;
+  width: 300px;
+  max-width: 100%;
+  max-height: 100%;
+  background-color: var(--background-i2);
+  overflow-y: auto;
+  max-height: 80%;
+}
+#content {
+  padding: 20px;
+
+  margin-top: 15px;
+}
+#closeBtn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  aspect-ratio: 1;
+  height: 30px;
+  padding: 0;
+  border-radius: 0;
+}
+</style>
--- src/views/users/comps/drawer/Drawer.vue
+++ src/views/users/comps/drawer/Drawer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="drawer" ref="drawer" @blur="onBlur" tabindex="0">
-    <button>Update user's role</button>
+    <button @click="onChangeRole">Change user's role</button>
     <button
       v-if="bannedOn == null"
       :style="{ color: 'red' }"
@@ -12,6 +12,7 @@
       Remove from blacklist
     </button>
   </div>
+  <ChangeUserRole v-if="isChangingRole" />
 </template>
 
 <script>
@@ -20,6 +21,9 @@ import { onMounted, ref } from "vue";
 import banUnbanPhoneNum from "../../apis/banUnbanPhoneNum";
 import { useRouter } from "vue-router";
 
+// comps
+import ChangeUserRole from "./ChangeUserRole.vue";
+
 export default {
   name: "Drawer-",
   props: {
@@ -27,9 +31,15 @@ export default {
     phoneNum: String,
     bannedOn: Number,
   },
+  components: {
+    ChangeUserRole,
+  },
   setup(props) {
     const router = useRouter();
     const drawer = ref(null);
+
+    // status
+    const isChangingRole = ref(false);
 
     // INPUT
     const onBlur = (event) => {
@@ -54,13 +64,17 @@ export default {
       }
     };
 
+    const onChangeRole = () => {
+      isChangingRole.value = true;
+    };
+
     // LIFECYCLE
     onMounted(() => {
       // when start, focus the drawer div
       drawer.value.focus();
     });
 
-    return { drawer, onBlur, onBanUnban };
+    return { drawer, onBlur, onBanUnban, isChangingRole, onChangeRole };
   },
 };
 </script>
