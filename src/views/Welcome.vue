8662fa69f33293cd944fc025ed96ca2ff66ecb7f
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-06-30 20:56:19 -0400

    q

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -6,19 +6,84 @@
         <button class="orangeBtn" @click="toBookNow">Book Now</button>
       </div>
     </div>
-    <img :src="img1" alt="Logo" />
+
+    <img :src="images[index]" alt="Logo" @load="onImgLoad" :style="imgstyle" />
   </div>
 </template>
 
 <script setup>
 import img1 from "@/assets/1.jpg";
+import img2 from "@/assets/2.jpg";
+import img3 from "@/assets/3.jpg";
+import img4 from "@/assets/4.jpg";
+import img5 from "@/assets/5.jpg";
+import img6 from "@/assets/6.jpg";
+import img7 from "@/assets/7.jpg";
+import img8 from "@/assets/8.jpg";
+import img9 from "@/assets/9.jpg";
+import img10 from "@/assets/10.jpg";
+import img11 from "@/assets/11.jpg";
+
 import { useRouter } from "vue-router";
-// resources
+import { onMounted, onUnmounted, ref } from "vue";
+
+// CONSTANTS
 const router = useRouter();
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+];
+
+const styles = [
+  "object-position: 100% 25%;",
+  "object-position: 80% 50%;",
+  "object-position: 30% 30%;",
+  "object-position: 0% 32%;",
+  "object-position: 0% 50%;",
+  "object-position: 60% 73%;",
+  "object-position: 60% 40%;",
+  "object-position: 100% 45%;",
+  "object-position: 50% 50%;",
+  "object-position: 50% 45%;",
+  "object-position: 50% 50%;",
+];
+
+// RESOURCES
+const index = ref(Math.floor(Math.random() * 11));
+// const index = ref(5);
+const imgstyle = ref("");
 
 // INPUT
 function toBookNow() {
   router.push("/booknow");
+}
+
+// LIFECYCLE
+let intervalId = null;
+onMounted(() => {
+  intervalId = setInterval(() => {
+    index.value++;
+    if (index.value > 10) {
+      index.value = 0;
+    }
+  }, 5000);
+});
+
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
+
+function onImgLoad() {
+  imgstyle.value = styles[index.value];
 }
 </script>
 
@@ -31,7 +96,7 @@ img {
   width: 100%;
   height: calc(100vh - 60px);
   object-fit: cover;
-  object-position: 100% 25%;
+  /* object-position: 100% 25%; */
   display: block;
   filter: brightness(50%);
 }
@@ -63,7 +128,7 @@ button {
   border-radius: 20px;
   padding: 10px 30px;
   font-size: 20px;
-  background-color: rgba(250, 166, 31, 0.7);
+  background-color: rgba(250, 166, 31, 0.75);
 }
 @media (orientation: portrait) {
   #slogan {
